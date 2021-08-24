d923307f27115ddb6af02d67b27d77134bcc2d82
Alperen Kapusuz <63202119+alperenkapusuz@users.noreply.github.com>
2021-08-24 15:51:12 +0300

    firestore içeriği menude gösterildi

--- src/components/mainmenu/products/ProductList.js
+++ src/components/mainmenu/products/ProductList.js
@@ -1,11 +1,48 @@
 import { useEffect, useState } from "react";
-import { Table, Button } from "reactstrap";
-import { db, fs } from "../../authentication/firebase/Firebase";
+import { db } from "../../authentication/firebase/Firebase";
+import { Table } from "reactstrap";
 
 function ProductList() {
-  
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    db.collection("items").onSnapshot((snapshot) => {
+      setItems(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  }, []);
+  console.log(items);
+
   return (
-    <div>Hello World</div>
+    <div>
+      <Table striped>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>BRAND</th>
+            <th>NAME</th>
+            <th>PRICE</th>
+            <th>QUENTITY</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {items.map(({ id, data: { brand, name, price, quentity } }) => (
+            <tr>
+              <th scope="row">{id}</th>
+              <td>{brand}</td>
+              <td>{name}</td>
+              <td>{price}</td>
+              <td>{quentity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 }
 
